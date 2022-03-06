6bc4c48ee2d9099fcf1ea64a5acd794a07e3c15b
joaovictor9162@outlook.com <joaovictor9162@outlook.com>
2022-03-06 14:36:23 -0300

    Atualizações

--- Aplicação/Dono/update.js
+++ Aplicação/Dono/update.js
@@ -40,6 +40,8 @@ module.exports = {
     //Código aqui:
 
     let client = interaction.options.getString('client');
+    let avatar = interaction.client.user.displayAvatarURL({ dynamic: true, format: 'png', size: 4096 });
+    let username = interaction.client.user.username;
 
     if (client == 'username') {
       await interaction.reply('Insira o novo nome:');
@@ -51,21 +53,22 @@ module.exports = {
         let collected = i.content;
 
         interaction.client.user.setUsername(collected)
-        .then(() => interaction.reply(`O nome do cliente foi atualizado com sucesso para: \`${client.user.username}\`.`))
-        .catch((e) => {
-          console.error(e);
-          return interaction.reply('Ocorreu um erro, tente novamente mais tarde');
-        });
+          .then(() => interaction.reply(`O nome do cliente foi atualizado com sucesso para: \`${username}\`.`))
+          .catch((e) => {
+            console.error(e);
+            return interaction.reply('Ocorreu um erro, tente novamente mais tarde');
+          });
       });
 
       collector.on('end', () => {
         interaction.followUp('Acabou o tempo.');
       });
-      
+
     } else if (client == 'avatar') {
       await interaction.reply('Insira um link ou anexo:');
 
-      let filter = (i) => i.author.id === interaction.user.id;
+      let filter = (i) => i.author.id === interaction.user.id;let avatar = interaction.client.user.displayAvatarURL({ dynamic: true, format: 'png', size: 4096 });
+      let username = interaction.client.user.username;
       let collector = interaction.channel.createMessageCollector({ filter: filter, time: 60000 * 5, max: 1, errors: ['time'] });
 
       collector.on('collect', async (i) => {
@@ -78,14 +81,15 @@ module.exports = {
         }
 
       });
-      
-      collector.on('end', () => {
+
+      collector.on('end', (c, r) => {
+        if (r == 'time') {
         interaction.followUp('Acabou o tempo.');
+        }
       });
-      
+
     } else {
-      let avatar = interaction.client.user.displayAvatarURL({ dynamic: true, format: 'png', size: 4096 });
-      let username = interaction.client.user.username;
+
 
       let embed = new MessageEmbed()
         .setColor(process.env.EMBED_COLOR)
