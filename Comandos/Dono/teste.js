0dd2ef6f254bd1c63eaed1297869dab9b858b0e9
joaovictor9162@outlook.com <joaovictor9162@outlook.com>
2022-03-26 09:30:37 -0300

    Update

--- Comandos/Dono/teste.js
+++ Comandos/Dono/teste.js
@@ -11,14 +11,26 @@ module.exports = {
    * @param {string[]} args
    */
   commandExecute: (client, message, args) => {
-    
+
     if (message.author.id !== process.env.OWNER_ID) {
       return;
+    }
+
+    // Código aqui:
+
+    message.channel.send('Testando');
+
+  },
+
+  interactionExecute: (interaction) => {
+    
+    if (interaction.user.id !== process.env.OWNER_ID) {
+      return
     }
     
     // Código aqui:
     
-    message.channel.send('Testando');
-
+    console.log(interaction.message)
+    interaction.deferUpdate();
   }
 }
