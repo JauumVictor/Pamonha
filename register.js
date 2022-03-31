d83b7272c9930065b880266bfb89634c33d8b847
joaovictor9162@outlook.com <joaovictor9162@outlook.com>
2022-03-30 23:20:03 -0300

    Update

---
+++ register.js
@@ -0,0 +1,19 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
+
+//===============> Atualizações dos comandos de barra <===============//
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+(async () => {
+  try {
+    console.log(c.yellow('Atualizando os comandos de barra (/).'));
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID), { body: commands },
+    );
+
+    console.log(c.green('Atualizado com sucesso todos os comandos de barra (/)!'));
+  } catch (error) {
+    console.error(error);
+  }
+})();
