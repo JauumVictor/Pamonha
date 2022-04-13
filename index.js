c30c9e7e1f6edb3b3936e39414727e44a5440e4e
joaovictor9162@outlook.com <joaovictor9162@outlook.com>
2022-04-13 09:05:13 -0300

    Update

--- index.js
+++ index.js
@@ -57,13 +57,13 @@ for (const folder of commandsFolders) {
   const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
   (async () => {
     try {
-      console.log(c.yellow('Atualizando os comandos de barra (/).'));
+      console.log(yellow('Atualizando os comandos de barra (/).'));
 
       await rest.put(
         Routes.applicationCommands(process.env.CLIENT_ID), { body: commands },
       );
 
-      console.log(c.green('Atualizado com sucesso todos os comandos de barra (/)!'));
+      console.log(green('Atualizado com sucesso todos os comandos de barra (/)!'));
     } catch (error) {
       console.error(error);
     }
